2eea98edd39fadf314a6518c650d032f1bfa11cb
manux1337 <tagwriter@mail.ru>
2014-10-03 11:54:16 +0600

    Переделал

--- n1/n1/Program.cs
+++ n1/n1/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.IO;
 namespace n1
 {
@@ -10,74 +9,33 @@ namespace n1
         static void Main(string[] args)
         {
             Console.Title = "Task1-#1";
-            Point tx = new Point();
-            if ((args.Length > 0) && (args[0] == "--file"))
+            string _text = "";
+            if (args.Length > 0)
             {
-                tx.FromFile(args[1]);
-            }
-            else
-            {
-                Console.Write("Введите координаты X, Y в формате {0.00},{0.00} = ");
-                tx.text = Console.ReadLine();
-            }
-
-                if (tx.success)
- 
-                    Console.WriteLine("\nX:{0}; Y:{1}", tx.x, tx.y);
+                if (File.Exists(args[1]))
+                {
+                    StreamReader fs = new StreamReader(@args[1]);
+                    _text = ((fs.ReadLine()) != null)?fs.ReadLine():"0,0";
+                }
                 else
-                    Console.WriteLine(tx.error);
+                {
+                    Console.WriteLine("Файл не найден...");
+                    
+                }
+
+            }
+                else 
+                {
+                     Console.Write("Введите координаты X, Y = ");
+                     _text = Console.ReadLine();
+                }
+            int ln = _text.Split(',').Length;
+            string[] _pts =(ln>2||ln==0)?new string[2]{"0","0"}:_text.Split(',');
+            Console.WriteLine("X:{0}; Y:{1};",_pts[0],_pts[1]);
             Console.WriteLine("\nНажмите любую клавишу для закрытия...");
             Console.ReadKey();
         }
     }
 
-    class Point
-    {
-        private string ptrn = @"(\w+\.\w+)\,(\w+\.\w+)";
-        private string _text;
-        public string x,y;
-        public bool success;
-        public string error = null;
-        public void FromFile(string path)
-        {
-            if (File.Exists(path))
-            {
-                StreamReader fs = new StreamReader(path);
-                this.text = fs.ReadLine();
-            }
-            else
-            {
-                this.error = "Ошибка!\nФайл не найден.";
-            }
-        }
-        private void encodeIt()
-        {
-            Regex regexp = new Regex(this.ptrn);
-            Match _matched = regexp.Match(this._text);
-            if (_matched.Success)
-            {
-                this.x = _matched.Groups[1].Value;
-                this.y = _matched.Groups[2].Value;
-                this.success = true;
-            }
-            else
-            {
-                this.success = false;
-                this.error = "Ошибка!\nДанные введены не верно.";
-            }
-        }
-        public string text
-        {
-            get
-            {
-                return this._text;
-            }
-            set
-            {
-                this._text = value;
-                this.encodeIt();
-            }
-        }
-
-    }
+  
 }
